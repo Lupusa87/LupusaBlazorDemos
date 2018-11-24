29ecba158dc2b7f4c815ee664596145b8ac9a9eb
Lupusa87 <VakhtangiAbashidze@gmail.com>
2018-11-23 22:36:56 -0500

    11/23/2018 1

---
+++ BlazorApp1/Class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1
+{
+    public class Class:BlazorComponent
+    {
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+
+    }
+}
