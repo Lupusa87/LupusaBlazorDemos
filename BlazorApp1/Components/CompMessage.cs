288eaed08bda5aee73493a55ca7c254db38809d0
Lupusa87 <VakhtangiAbashidze@gmail.com>
2019-01-22 14:41:57 -0500

    1/22/2019 2:41

--- BlazorApp1/Components/CompMessage.cs
+++ BlazorApp1/Components/CompMessage.cs
@@ -4,65 +4,109 @@ using Microsoft.AspNetCore.Blazor.RenderTree;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using static BlazorWebSocketHelper.Classes.BwsEnums;
 
-namespace BlazorApp1.Components
+namespace BlazorWebSocketWebWorker.Client.Components
 {
 
-        public class CompMessage : BlazorComponent, IDisposable
+
+    public class CompMessage : BlazorComponent, IDisposable
+    {
+
+        [Parameter]
+        protected BwsMessage bwsMessage { get; set; }
+
+
+        [Parameter]
+        protected BlazorComponent parent { get; set; }
+
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
 
-            [Parameter]
-            protected BwsMessage bwsMessage { get; set; }
+
+            int k = -1;
 
 
-            [Parameter]
-            protected BlazorComponent parent { get; set; }
+
+            builder.OpenElement(k++, "div");
+            builder.AddAttribute(k++, "id", "listitem" + bwsMessage.ID); // + Guid.NewGuid().ToString("d").Substring(1, 4));
+            builder.AddAttribute(k++, "style", "width:400px;max-height:26px;position:relative;margin:5px");
+            builder.OpenElement(k++, "span");
 
 
-            protected override void BuildRenderTree(RenderTreeBuilder builder)
+
+
+
+            if (bwsMessage.MessageType == BwsMessageType.send)
             {
+                builder.AddAttribute(k++, "style", "position:absolute;top:0px;cursor:pointer;color:blue");
+            }
+            else
+            {
+                builder.AddAttribute(k++, "style", "position:absolute;top:0px;cursor:pointer;color:green");
+            }
 
-
-                int k = -1;
-
-
-
-                builder.OpenElement(k++, "div");
-                builder.AddAttribute(k++, "id", "listitem" + bwsMessage.ID); // + Guid.NewGuid().ToString("d").Substring(1, 4));
-                builder.AddAttribute(k++, "style", "width:400px;max-height:26px;position:relative;margin:5px");
-                builder.OpenElement(k++, "span");
-
-
-
-
-
-                if (bwsMessage.MessageType == BwsEnums.BwsMessageType.send)
-                {
-                    builder.AddAttribute(k++, "style", "position:absolute;top:0px;cursor:pointer;color:blue");
-                }
-                else
-                {
-                    builder.AddAttribute(k++, "style", "position:absolute;top:0px;cursor:pointer;color:green");
-                }
-
-                builder.AddContent(k++, bwsMessage.ID + " " + bwsMessage.Date.ToString("HH:mm:ss.fff") + " " + bwsMessage.MessageType.ToString() + ": " + bwsMessage.Message);
-
-                builder.CloseElement();
-
-                builder.CloseElement();
-
-
-                base.BuildRenderTree(builder);
+            switch (bwsMessage.TransportType)
+            {
+                case BwsTransportType.Text:
+                    builder.AddContent(k++, bwsMessage.ID + " " +
+                        bwsMessage.Date.ToString("HH:mm:ss.fff") + " " +
+                        bwsMessage.MessageType.ToString() + " " +
+                        bwsMessage.TransportType.ToString().ToLower() + ": " +
+                        bwsMessage.Message);
+                    break;
+                case BwsTransportType.ArrayBuffer:
+                    builder.AddContent(k++, bwsMessage.ID + " " +
+                        bwsMessage.Date.ToString("HH:mm:ss.fff") + " " +
+                        bwsMessage.MessageType.ToString() + " " +
+                        bwsMessage.TransportType.ToString().ToLower() + ": " +
+                        Encoding.UTF8.GetString(bwsMessage.MessageBinary) +
+                        " [" + ByteArrayToVisualString(bwsMessage.MessageBinary) + "]");
+                    break;
+                case BwsTransportType.Blob:
+                    break;
+                default:
+                    break;
             }
 
 
 
-            public void Dispose()
+            builder.CloseElement();
+
+            builder.CloseElement();
+
+
+            base.BuildRenderTree(builder);
+        }
+
+        private string ByteArrayToVisualString(byte[] par_b)
+        {
+
+            if (par_b.Length > 0)
             {
+                StringBuilder s = new StringBuilder();
+                for (int i = 0; i < par_b.Length; i++)
+                {
+                    s.Append(par_b[i] + ",");
+                }
+                s.Remove(s.Length - 1, 1);
+
+                return s.ToString();
 
             }
+
+
+            return string.Empty;
+
+
+        }
+
+        public void Dispose()
+        {
+
         }
     }
-
-
+}
